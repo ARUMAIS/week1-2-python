2a39dd6fd391cefd3072a996398feeac85985549
Aishwaryalakshmi Arumugam <aishwaryalakshmi.a@gmail.com>
2021-09-24 11:23:14 +0100

    q3

---
+++ questions/q1.py
@@ -0,0 +1,19 @@
+print("\t"*2+"Title")
+print("Hello")
+print("Heloo World")
+a=5
+b=12.3456789
+c=a+b
+d="A number" +b
+e=a +\
+    b +/
+c
+    print(a)
+f=2
+g=a/f
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
---
+++ questions/q3.py
@@ -0,0 +1,11 @@
+# . ppsn # alphanumeric / string
+# . passport number # alphanumeric / string
+# . temperature in degrees # float
+# . CAO number # integer
+# . Miles traveled per day # float
+# . Average collections of exam grades #list
+# . File path #string
+# . persons middles initial # string
+# . Biography statement # string
+# . Book abstract # string
+# . Mathematical function # complex numbers
