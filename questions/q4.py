22f27f46e895631b4229608f7ee0f98c09324871
Aishwaryalakshmi Arumugam <aishwaryalakshmi.a@gmail.com>
2021-09-24 22:58:13 +0100

    q4 comments

--- questions/q4.py
+++ questions/q4.py
@@ -1,3 +1,16 @@
-Ans_1 = 6+8/2-1
+ans_1 = 6 + 8 / 2 - 1 #float
 print(ans_1)
-ans_2 = 1+1-1=2
+ans_2 = 1 + 1 - 1 + 2 / 2 # answer is in float
+print(ans_2)
+ans_3 = 1.1 + 5 / 2 #answer is in float
+print(ans_3)
+ans_4 = 9.2 / 3
+print(ans_4)
+ans_5 = 9.2 % 3
+print(ans_5)
+ans_6 = 1 * 2 + 3 * 5 % 4 #precedence to %
+print(ans_6)
+ans_7 = 1 + 8 % 3 * 2 - 9 #precedence to %
+print(ans_7)
+ans_8 = 6 + ( 24 % (16 - 11)) #precendence to bracket
+print(ans_8)
